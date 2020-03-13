61c934958e654fc0d7046b21b12d9e69bb7cb891
neilZon <neilcviloria@gmail.com>
2020-03-13 13:16:11 -0600

    added staticfiles

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,5 +148,4 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
+django_heroku.settings(locals())
